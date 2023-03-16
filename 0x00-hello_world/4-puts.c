2f6a32d80cfb247c38575d62883996e2e2c77222
boateng eric <boateng.eric44@gmail.com>
2023-03-16 20:32:51 +0000

    4.puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
+/** This program was created by Eric Boateng
+ * during his study at ALX
+ */
 #include <stdio.h>
+/**
+ *Programming is like building a multilingual puzzle followed by a new line
+ *
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-}
 	return (0);
+}
