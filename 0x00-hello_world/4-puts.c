df4a6b494e3d3b643175729a2aba816814e620d5
boateng eric <boateng.eric44@gmail.com>
2023-03-16 20:45:51 +0000

    4.puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,13 +7,8 @@
  * Programming is like building a multilingual 
  * puzzle followed by a new line
  */
-
 int main(void)
- /* This is the main*/
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
-	/*Returns nothing*/
 	return (0)
 }
