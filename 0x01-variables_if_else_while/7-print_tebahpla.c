6fd56a9725ba80f9d710f6704f517a6309d93ef2
boateng eric <boateng.eric44@gmail.com>
2023-03-18 05:29:55 +0000

    printing lowercase in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+/*
+ * Auth: Eric Kwakye Boateng
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabet in reverse.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+		putchar(alphabet);
+
+	putchar('\n');
+
+	return (0);
+}
