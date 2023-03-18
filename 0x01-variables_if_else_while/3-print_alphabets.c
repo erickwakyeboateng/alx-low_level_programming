c1a81dd16a3c142e094b8c9c72aca3f5d99b9ae2
boateng eric <boateng.eric44@gmail.com>
2023-03-18 05:15:56 +0000

    capital alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+/*
+ * Auth: Eric Kwakye Boateng
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(letter);
+
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
