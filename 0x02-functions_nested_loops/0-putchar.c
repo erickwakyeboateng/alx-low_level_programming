c58a83afe469ec7382b7399bd2613d324d22f896
boateng eric <boateng.eric44@gmail.com>
2023-03-21 12:16:08 +0000

    _putcharr

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,11 @@
 /*Auth: Eric Kwakye Boateng
  *Task: Printing _putchar
  */
-#include "main.h"
+#include"main.h"
 /**
  * Main -  prints _putchar, followed by a new line.
  */
 int main(void)
-{
 	_putchar("_putchar\n");
 	return (0);
-}
 
