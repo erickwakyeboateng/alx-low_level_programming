f90e3651a520418248b976df4d0fbab38c3cf170
boateng eric <boateng.eric44@gmail.com>
2023-03-21 14:32:56 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,7 +13,7 @@ void print_alphabet_x10(void)
 
 	while (counter++ <= 9)
 	{
-		for (alpha = 'a'; alpha <= 'z'; alpha ++)
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
 			_putchar(alpha);
 		_putchar('\n');
 	}
