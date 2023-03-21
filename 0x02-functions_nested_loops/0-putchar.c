593807bb9686a3d962e5c7e3e1cc7e854105aea7
boateng eric <boateng.eric44@gmail.com>
2023-03-21 10:57:25 +0000

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+/*Auth: Eric Kwakye Boateng
+ *Task: Functions and Nested Loops
+ */
+#include <stdio.h>
+/*
+ * Main -  prints _putchar, followed by a new line.
+ */
+int main(void)
+{
+	_putchar("_putchar\n");
+	return (0);
+}
+
