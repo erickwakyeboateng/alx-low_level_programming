0096daf49992b5235d61795faf7277ea1c96f6d0
boateng eric <boateng.eric44@gmail.com>
2023-03-21 12:28:39 +0000

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,13 @@
-/*Auth: Eric Kwakye Boateng
- *Task: Printing _putchar
- */
-#include <main.h>
-/**
- * Main -  prints _putchar, followed by a new line.
- */
-int main(void)
-	_putchar("_putchar\n");
-	return (0);
+#include <unistd.h>
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
