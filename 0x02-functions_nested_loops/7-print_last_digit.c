ec6a68e6f23e71690857d42fd5cd7e203e148ef9
boateng eric <boateng.eric44@gmail.com>
2023-03-21 16:17:42 +0000

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+/*
+ * Auth: Eric Kwakye Boateng
+ * Task: 7-print_last_digit.c
+ */
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number.
+ * @n: The number in question
+ * Return: Returns the last digit.
+ */
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
+	_puthar(last_digit + '0');
+
+	return (last_digit);
+
+}
