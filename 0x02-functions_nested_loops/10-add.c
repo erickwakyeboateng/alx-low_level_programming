b161c00d39589e42e3bbee4df6a36c2d4b451c53
boateng eric <boateng.eric44@gmail.com>
2023-03-21 17:44:06 +0000

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+/*
+ * Auth: Eric Kwakye Boateng
+ * Task: 10-add.c
+ */
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ * @a:  first integer to add 
+ * @b: second integer to add
+ * Return: Return sum of and b
+ */
+int add (int a, int b)
+{
+	return (a + b);
+
+}
