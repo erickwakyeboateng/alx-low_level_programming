55d2d176807b7569da6cc4a992d1d3dc2c5a860a
boateng eric <boateng.eric44@gmail.com>
2023-03-23 18:17:07 +0000

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line: prints a line 
+ */
+void print_line(int n)
+{
+	int line;
+
+	if (n > 0)
+	{
+		for (line = 0; line < n; line++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
