f63022a643338c76037eb88f8b3462453d1f7409
boateng eric <boateng.eric44@gmail.com>
2023-03-23 16:05:25 +0000

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - check the code
+ *@i: variable to be checked
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar("%d\n", i);
+
+}
