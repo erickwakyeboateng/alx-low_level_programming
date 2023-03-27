d578797ebbca50ceca4f0b20e16e966a5c9229fa
boateng eric <boateng.eric44@gmail.com>
2023-03-27 19:58:13 +0000

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,19 +8,19 @@
  */
 int _atoi(char *s)
 {
-	int i, j, k, len, f, digit;
+	int i, j, k, len, m, digit;
 
 	i = 0;
 	j = 0;
 	k = 0;
 	len = 0;
-	f = 0;
+	m = 0;
 	digit = 0;
 
 	while (s[len] != '\0')
 		len++;
 
-	while (i < len && f == 0)
+	while (i < len && m == 0)
 	{
 		if (s[i] == '-')
 			++j;
@@ -30,18 +30,18 @@ int _atoi(char *s)
 			digit = s[i] - '0';
 			if (j % 2)
 				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
+			k = k * 10 + digit;
+			m = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
-			f = 0;
+			m = 0;
 		}
 		i++;
 	}
 
-	if (f == 0)
+	if (m == 0)
 		return (0);
 
-	return (n);
+	return (k);
 }
 
