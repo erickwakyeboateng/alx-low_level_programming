791c03d6a16a115f77eaedfa149ed824056bc624
boateng eric <boateng.eric44@gmail.com>
2023-04-05 15:35:16 +0000

    0x08-recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the lenght of string
+ *
+ * @s: character argument
+ *
+ * Return: int
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+		return (0)
+}
