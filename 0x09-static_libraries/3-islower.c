39c146c9f53a2b429a89ff32da79f1dd5e227cd0
boateng eric <boateng.eric44@gmail.com>
2023-04-06 14:02:32 +0000

    C - Static libraries

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * _islower - checks for lowercase character
+  *
+  * @c: is a letter, lowercase or uppercase
+  *
+  * Return: On 0 or 1
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
