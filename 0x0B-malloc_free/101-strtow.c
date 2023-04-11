42eb93bb2996ea72a2d0e2e0e0d94c2b1950119f
boateng eric <boateng.eric44@gmail.com>
2023-04-11 17:00:11 +0000

    debugging

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *count_word - helper function to count the number of words in a string
+ *word_count - helper function to count the number of words in a string
  *@s: string to evaluate
  *Return: number of words
  **/
