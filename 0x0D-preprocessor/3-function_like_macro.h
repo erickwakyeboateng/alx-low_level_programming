68b2b2ebb1286679c2bfa33fffc35b2b949139f0
boateng eric <boateng.eric44@gmail.com>
2023-04-17 14:13:14 +0000

    Function-like macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/*
+ * Header file that computes the absolute value of a number x
+ */
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif
