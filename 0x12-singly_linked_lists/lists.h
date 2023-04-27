bc18d94317fcff4057a0ef7ca360e38df9795c94
boateng eric <boateng.eric44@gmail.com>
2023-04-27 16:03:10 +0000

    add_node

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
-
-
 #include <unistd.h>
 /**
  * struct list_s - singly linked list
@@ -22,6 +19,8 @@ typedef struct list_s
 } list_t;
 
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
