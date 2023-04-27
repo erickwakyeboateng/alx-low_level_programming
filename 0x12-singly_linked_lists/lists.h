fc4f0581e2d1f61300b04f4a5224002f37849d5f
boateng eric <boateng.eric44@gmail.com>
2023-04-27 15:52:00 +0000

    fixing bugs

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdio.h>
+
+
 #include <unistd.h>
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
