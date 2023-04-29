1c11777005e89074356dcf9e48390e70af2de40f
Eric Kwakye Boateng <84413016+erickwakyeboateng@users.noreply.github.com>
2023-04-29 18:38:22 +0000

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,14 +6,14 @@
  */
 void free_list(list_t *head)
 {
-	list_t *tmp;
+	list_t *temp;
 
 	while (head)
 	{
-		tmp = head->next;
+		temp = head->next;
 		free(head->str);
 		free(head);
-		head = tmp;
+		head = temp;
 	}
 }
 
