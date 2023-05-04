726a3a5349a60703bcfef78d8cfaf71458c0f822
boateng eric <boateng.eric44@gmail.com>
2023-05-04 14:39:40 +0000

    1.1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,7 +4,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 #endif
