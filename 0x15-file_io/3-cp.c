0afee8c411a555ed66b88e49d1b04308a9dddf93
boateng eric <boateng.eric44@gmail.com>
2023-05-08 17:32:21 +0000

    fixing

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,40 +1,43 @@
 #include "main.h"
-char *krate_buffer(char *file);
-void klose_file(int fd);
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
- * krate_buffer - Allocates 1024 bytes for a buffer.
+ * create_buffer - Allocates 1024 bytes for a buffer.
  * @file: The name of the file buffer is storing chars for.
  *
  * Return: A pointer to the newly-allocated buffer.
  */
-char *krate_buffer(char *file)
+char *create_buffer(char *file)
 {
-	char *buf;
+	char *buffer;
 
-	buf = malloc(sizeof(char) * 1024);
+	buffer = malloc(sizeof(char) * 1024);
 
-	if (buf == NULL)
+	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
 			"Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
-	return (buf);
+	return (buffer);
 }
 
 /**
- * klose_file - Closes file descriptors.
+ * close_file - Closes file descriptors.
  * @fd: The file descriptor to be closed.
  */
-void klose_file(int fd)
+void close_file(int fd)
 {
-	int clos;
+	int c;
 
-	clos = close(fd);
+	c = close(fd);
 
-	if (clos == -1)
+	if (c == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
@@ -44,19 +47,19 @@ void klose_file(int fd)
 /**
  * main - Copies the contents of a file to another file.
  * @argc: The number of arguments supplied to the program.
- * @argv: Array of pointers to the arguments.
+ * @argv: An array of pointers to the arguments.
  *
  * Return: 0 on success.
  *
- * Note: If the argument count is incorrect - exit code 97.
- *       If file_from does not exist or cannot be read - exit code 98.
- *       If file_to cannot be created or written to - exit code 99.
- *       If file_to or file_from cannot be closed - exit code 100.
+ * Description: If the argument count is incorrect - exit code 97.
+ *              If file_from does not exist or cannot be read - exit code 98.
+ *              If file_to cannot be created or written to - exit code 99.
+ *              If file_to or file_from cannot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
 {
-	int f_rom, to_, r_ead, rite;
-	char *buf;
+	int from, to, r, w;
+	char *buffer;
 
 	if (argc != 3)
 	{
@@ -64,35 +67,37 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	buf = krate_buffer(argv[2]);
-	f_rom = open(argv[1], O_RDONLY);
-	r_ead = read(f_rom, buf, 1024);
-	to_ = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (f_rom == -1 || r_ead == -1)
+		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't read f_rom file %s\n", argv[1]);
-			free(buf);
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
 			exit(98);
 		}
 
-		rite = write(to_, buf, r_ead);
-		if (to_ == -1 || rite == -1)
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't write to_ %s\n", argv[2]);
-			free(buf);
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
 			exit(99);
 		}
 
-		r_ead = read(f_rom, buf, 1024);
-		to_ = open(argv[2], O_WRONLY | O_APPEND);
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
 
-	} while (r_ead > 0);
-	free(buf);
-	klose_file(f_rom);
-	klose_file(to_);
+	} while (r > 0);
+
+	free(buffer);
+	close_file(from);
+	close_file(to);
+
 	return (0);
 }
