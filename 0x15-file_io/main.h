0227afc2e3a24863db694295446d0a91b23317e0
boateng eric <boateng.eric44@gmail.com>
2023-05-09 11:18:04 +0000

    elf header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,4 +14,20 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 char *create_buffer(char *file);
 void close_file(int fd);
+
+
+/*ELF HEADER TASK*/
+#include <elf.h>
+#include <fcntl.h>
+
+void CheckElf(unsigned char *e_identifier);
+void PrintMagic(unsigned char *e_identifier);
+void PrintClass(unsigned char *e_identifier);
+void PrintData(unsigned char *e_identifier);
+void PrintVersion(unsigned char *e_identifier);
+void PrintAbi(unsigned char *e_identifier);
+void PrintOsabi(unsigned char *e_identifier);
+void PrintType(unsigned int eType, unsigned char *e_identifier);
+void PrintEntry(unsigned long int eEntry, unsigned char *e_identifier);
+void CloseElf(int elf);
 #endif
